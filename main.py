b143e9450637f4993e67196f78c016d8b73a827e
Abhishek Chauhan <bbiilluu9211@gmail.com>
2020-11-25 18:34:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#Encodeing and Decoding Programe.
+#Encode and decode your message.
+
+from art import logo
+print(logo)
+print("ALERT!  Don't give shift 25 or multipilcant of 25")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#def function which can encode and decode acording to provided inputs
+    
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    
+    if char in alphabet:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    else:
+        end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+#creating a loop so that you can perform task continuesly until you type no 
+should_continue = True
+while should_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 25
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == 'no':
+        should_continue = False
+        print('Goodbye!')
+    else:
+        should_continue = True
+        
